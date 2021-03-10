702e79b72a106d3e1f297fa1d0b8046f2347558f
john_atha <giannisj3@gmail.com>
2021-03-10 14:41:33 +0200

    Update admin.py to show classes

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+class EmailAdmin(admin.ModelAdmin):
+    pass
+
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Email, EmailAdmin)
